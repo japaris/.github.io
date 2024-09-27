339aad7629b01345f74327a2b1347bcaaeaee085
japaris <jan.tumpach@free.fr>
2024-09-27 14:16:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Le script JavaScript fonctionne !");
+document.getElementById("downloadBtn").onclick = function() {
+  const videoUrl = document.getElementById("videoUrl").value;
+  
+  if (videoUrl) {
+    // Afficher un message de succès (ou un appel API pour le téléchargement)
+    document.getElementById("message").innerText = "Téléchargement en cours pour : " + videoUrl;
+  } else {
+    document.getElementById("message").innerText = "Veuillez entrer une URL valide.";
+  }
+};
